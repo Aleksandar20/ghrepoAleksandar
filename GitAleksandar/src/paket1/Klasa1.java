22746fdb185c2b73f448f9c1bb12d0f30e2595cc
aleksandar.korac <aleksandar.korac@VASTERA07.intens.local>
2018-03-13 12:13:27 +0100

    Aleksandar Brisanje gresaka

--- GitAleksandar/src/paket1/Klasa1.java
+++ GitAleksandar/src/paket1/Klasa1.java
@@ -6,11 +6,11 @@ public class Klasa1 {
 		// TODO Auto-generated method stub
 		System.out.println("poruka 1");
 		System.out.println("poruka 2");
-<<<<<<< HEAD
+
 		System.out.println("poruka 3");
-=======
+
 		System.out.println("poruka Djordje");
->>>>>>> branch 'master' of https://github.com/Aleksandar20/ghrepoAleksandar.git
+
 
 	}
 
